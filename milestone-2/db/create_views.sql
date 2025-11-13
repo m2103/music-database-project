afbd6bbc4b2be77e2540babe314154f7dd07754e
tanl1897 <145232709+laura838@users.noreply.github.com>
2025-11-13 01:06:00 -0500

    create song summary table for performance

---
+++ milestone-2/db/create_views.sql
@@ -0,0 +1,19 @@
+CREATE VIEW song_summary AS
+SELECT
+  s.songID,
+  s.name AS songName,
+  al.name AS albumName,
+  al.albumCover,
+  a.artists,
+  sr.avgRating,
+  sr.reviewCount
+FROM song s
+JOIN album al ON al.albumID = s.albumID
+LEFT JOIN song_review_summary sr ON sr.songID = s.songID
+LEFT JOIN (
+  SELECT sa.songID,
+         GROUP_CONCAT(DISTINCT ar.name ORDER BY ar.name SEPARATOR ', ') AS artists
+  FROM song_artist sa
+  JOIN artist ar ON ar.artistID = sa.artistID
+  GROUP BY sa.songID
+) a ON a.songID = s.songID;
